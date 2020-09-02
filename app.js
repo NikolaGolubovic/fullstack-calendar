624dd4597111cfa247c7d47726f2740a80478809
NikolaGolubovic <nikolagolubovic985@gmail.com>
2020-09-02 12:20:24 +0200

    uibuild

--- app.js
+++ app.js
@@ -20,6 +20,20 @@ app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
 app.use("/api/events", eventsRouter);
 
+let protected = ["transformed.js", "main.css", "favicon.ico"];
+
+app.get("*", (req, res) => {
+  let path = req.params["0"].substring(1);
+
+  if (protected.includes(path)) {
+    // Return the actual file(s)
+    res.sendFile(`${__dirname}/build/${path}`);
+  } else {
+    // Otherwise, redirect to /build/index.html
+    res.sendFile(`${__dirname}/build/index.html`);
+  }
+});
+
 app.use(middleware.errorHandler);
 
 module.exports = app;
