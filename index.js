3d1a1341833bde5816cde2d49138c37ba663b46b
NikolaGolubovic <nikolagolubovic985@gmail.com>
2020-09-02 12:24:58 +0200

    uibuild

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require("./app");
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3005;
 
 server.listen(PORT, () => {
   console.log("Server connected to the port " + PORT);
